2c670c9c38379059537ab7970c89948f0f8effe3
agnelnieves <agnelnieves@gmail.com>
2020-11-21 11:14:47 -0500

    feat(slices && components): Add CTASection slice && add Button component

---
+++ components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <prismic-link
+  :class="{ 'mx-auto': isCentered}"
+  class="text-white bg-blue-600 font-bold py-3 px-5 mb-4 sm:mb-0 rounded-full inline-block hover:bg-blue-700 box-content text-base mt-4"
+  :field="field">
+      <slot/>
+  </prismic-link>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    field: {
+      type: Object,
+      default: null
+    },
+    isCentered: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
--- slices/CtaSection/index.vue
+++ slices/CtaSection/index.vue
@@ -1,14 +1,18 @@
 <template>
-  <section class="section">
-    <prismic-rich-text :field="slice.primary.title" />
-    <prismic-rich-text :field="slice.primary.description" />
-    <prismic-link :field="slice.primary.ctaLink">
+    <section class="section py-20 px-4 bg-blue-100 text-center">
+    <prismic-rich-text class="text-center text-sm uppercase text-blue-600 font-semibold" :field="slice.primary.title" />
+    <prismic-rich-text class="text-center text-5xl font-bold max-w-2xl mx-auto leading-tight mb-4" :field="slice.primary.description" />
+    <Button is-centered :field="slice.primary.ctaLink">
       {{ slice.primary.ctaLabel }}
-    </prismic-link>
+    </Button>
   </section>
 </template>
 <script>
+import Button from "@/components/button.vue"
 export default {
+  components: {
+    Button
+  },
   props: {
     slice: {
       type: Object,
@@ -20,19 +24,3 @@ export default {
   },
 }
 </script>
-<style scoped>
-.section {
-  position: relative;
-  background: #F7F7F7;
-  color: #111;
-  padding: 4em;
-  text-align: center;
-}
-a {
-  color: #111;
-}
-.title {
-  margin-bottom: 2em;
-}
-</style>
-
