0a3bdba27a2ca931471e8b8d3e4795dde3b23e2b
agnelnieves <agnelnieves@gmail.com>
2020-11-21 16:04:49 -0500

    feat(slices): Add HeroSection slice styles

--- slices/HeroSection/index.vue
+++ slices/HeroSection/index.vue
@@ -1,15 +1,25 @@
 <template>
-  <section class="section text-red-400 p-5">
-    <prismic-rich-text :field="slice.primary.title" class="title" />
-    <prismic-rich-text :field="slice.primary.description" />
-    <prismic-image :field="slice.primary.image" />
-    <prismicLink :field="slice.primary.ctaLink">
-      {{ slice.primary.ctaLabel }}
-    </prismicLink>
+  <section class="overflow-x-hidden bg-gradient-to-t from-gray-200">
+    <div class="max-w-5xl py-10 mx-auto flex  sm:flex-row sm:text-left text-center flex-col justify-between items-center">
+      <div class="max-w-md p-4">
+        <prismic-rich-text :field="slice.primary.title" class="text-4xl sm:text-5xl leading-11 font-bold leading-tight mb-4" />
+        <prismic-rich-text :field="slice.primary.description" class=" text-lg text-gray-600 max-w-sm"/>
+        <Button :field="slice.primary.ctaLink">
+          {{ slice.primary.ctaLabel }}
+        </Button>
+      </div>
+      <div class="sm:max-w-xs md:max-w-sm p-4">
+        <prismic-image :field="slice.primary.image" class="rounded-3xl max-w-full" />
+      </div>
+    </div>
   </section>
 </template>
 <script>
+import Button from "@/components/Button.vue"
 export default {
+  components: {
+    Button
+  },
   props: {
     slice: {
       type: Object,
