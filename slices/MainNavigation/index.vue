3a7e33cf2c1d50356dd2254d62d29a7313d6005f
Agnel Nieves Fuentes <agnel_nievesfuentes@ultimatesoftware.com>
2020-11-21 00:07:20 -0500

    feat(slices): Add MainNavigation slice

---
+++ slices/MainNavigation/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <prismic-image :field="slice.primary.logo"/>
+    <prismic-link v-for="(link,i) in slice.items" :key="i" :field="link.link">
+      {{ link.label }}
+    </prismic-link>
+  </nav>
+</template>
+<script>
+export default {
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {}
+      }
+    }
+  }
+}
+</script>
--- slices/index.js
+++ slices/index.js
@@ -1,2 +1,3 @@
 export { default as HeroSection } from './HeroSection'
 export { default as CardDeck } from './CardDeck'
+export { default as MainNavigation } from './MainNavigation'
