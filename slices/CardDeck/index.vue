5b9ec0d6b09ab2b9c72f2f64b7c9f2931ac727cb
Agnel Nieves Fuentes <agnel_nievesfuentes@ultimatesoftware.com>
2020-11-21 01:45:59 -0500

    feat(slices): Add CardDeck styles

--- slices/CardDeck/index.vue
+++ slices/CardDeck/index.vue
@@ -1,13 +1,20 @@
 <template>
-  <section class="section">
-    <prismic-rich-text :field="slice.primary.title" class="title" />
-    <prismic-rich-text :field="slice.primary.description" />
-    <div v-for="(card,i) in slice.primary.items" :key="i">
-      <prismic-rich-text :field="card.cardTitle" />
-      <prismic-rich-text :field="card.cardDescription" />
-      <prismic-link :field="card.cardCTALink">
-        {{ card.cardCTALabel }}
-      </prismic-link>
+  <section class="section py-20">
+    <prismic-rich-text class="text-center text-sm uppercase text-blue-600 font-semibold" :field="slice.primary.title" />
+    <prismic-rich-text class="text-center text-5xl font-bold max-w-2xl mx-auto leading-tight mb-12" :field="slice.primary.description" />
+    <div ref="cardDeck">
+      <div class="flex box-content justify-between items-center relative text-base bg-white shadow-lg sm:rounded-2xl sm:p-5 max-w-3xl mx-auto sticky top-0  sm:text-lg text-center" v-for="(card,i) in slice.primary.items" :key="i">
+        <div class="px-5 box-content mx-auto">
+          <prismic-rich-text class="text-3xl leading-8 font-semibold" :field="card.cardTitle" />
+          <prismic-rich-text :field="card.cardDescription" />
+          <prismic-link class="text-white bg-blue-600  font-bold py-3 sm:rounded-full block hover:bg-blue-700 box-content text-base mt-4" :field="card.cardCTALink">
+            {{ card.cardCTALabel }}
+          </prismic-link>
+        </div>
+        <div class="block max-w-md">
+          <prismic-image class="max-w-full sm:rounded-2xl" :field="card.image" />
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -24,4 +31,3 @@ export default {
   },
 }
 </script>
-
