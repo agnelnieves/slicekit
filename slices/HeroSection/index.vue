11a2574857a4ee3e2a58eb1d5d8adaa9f9192c3f
Agnel Nieves Fuentes <agnel_nievesfuentes@ultimatesoftware.com>
2020-11-20 22:42:56 -0500

    feat(slices): Add HeroSection slice initial layout

---
+++ slices/HeroSection/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="section">
+    <prismic-rich-text :field="slice.primary.title" class="title" />
+    <prismic-rich-text :field="slice.primary.description" />
+    <prismic-image :field="slice.primary.image" />
+    <prismicLink :field="slice.primary.ctaLink">
+      {{ slice.primary.ctaLabel }}
+    </prismicLink>
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {}
+      }
+    }
+  },
+}
+</script>
+<style scoped>
+.section {
+  position: relative;
+  background: #F7F7F7;
+  color: #111;
+  padding: 4em;
+  text-align: center;
+}
+a {
+  color: #111;
+}
+.title {
+  margin-bottom: 2em;
+}
+</style>
+
