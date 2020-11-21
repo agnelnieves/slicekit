5a34033b5f4c575f32efe53801da3d408eda5665
Agnel Nieves Fuentes <agnel_nievesfuentes@ultimatesoftware.com>
2020-11-21 00:29:38 -0500

    feat(slices): Add MainFooter slice

---
+++ slices/MainFooter/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer>
+    <prismic-image :field="slice.primary.logo"/>
+    <prismic-rich-text :field="slice.primary.tagline" />
+
+    <span>
+    &copy; {{ new Date().getFullYear() }}
+    {{ slice.primary.companyName }}
+    </span>
+
+    <prismic-link v-for="(social,i) in slice.items" :key="i" :field="social.link">
+      {{ social.label }}
+    </prismic-link>
+  </footer>
+</template>
+<script>
+export default {
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {}
+      }
+    }
+  },
+}
+</script>
+
