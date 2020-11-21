040c835daefb7b312cdc396e0b3f0cbb30d52577
Agnel Nieves Fuentes <agnel_nievesfuentes@ultimatesoftware.com>
2020-11-21 00:13:04 -0500

    feat(slices): Add CtaSection slice

---
+++ slices/CtaSection/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="section">
+    <prismic-rich-text :field="slice.primary.title" />
+    <prismic-rich-text :field="slice.primary.description" />
+    <prismic-link :field="slice.primary.ctaLink">
+      {{ slice.primary.ctaLabel }}
+    </prismic-link>
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {}
+      }
+    }
+  },
+}
+</script>
+<style scoped>
+.section {
+  position: relative;
+  background: #F7F7F7;
+  color: #111;
+  padding: 4em;
+  text-align: center;
+}
+a {
+  color: #111;
+}
+.title {
+  margin-bottom: 2em;
+}
+</style>
+
--- slices/index.js
+++ slices/index.js
@@ -1,3 +1,4 @@
 export { default as HeroSection } from './HeroSection'
 export { default as CardDeck } from './CardDeck'
 export { default as MainNavigation } from './MainNavigation'
+export { default as CtaSection } from './CtaSection'
